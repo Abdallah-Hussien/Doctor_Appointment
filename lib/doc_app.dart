a53f8cf03a79fdf79dbc886342ee255fdfe3286d
Abdallah Hussien <abdallah.hussien.muhammad@gmail.com>
2026-03-23 20:04:17 +0200

    # set app routing and adding needed colors

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,10 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:doc_app/core/routing/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocApp extends StatelessWidget {
-  const DocApp({super.key});
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Doctor Appointment',
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
   }
 }
