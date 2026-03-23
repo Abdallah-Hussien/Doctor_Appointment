3ba11a0286e0057c3c340187d5577df55d6067a5
Abdallah Hussien <abdallah.hussien.muhammad@gmail.com>
2026-03-23 20:24:08 +0200

    # add screen util

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:doc_app/core/routing/router.dart';
+import 'package:doc_app/core/routing/routes.dart';
+import 'package:doc_app/core/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -11,10 +13,16 @@ class DocApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      designSize: Size(375, 812),
       minTextAdapt: true,
       child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
         debugShowCheckedModeBanner: false,
         title: 'Doctor Appointment',
+        initialRoute: Routes.onBoardingScreen,
         onGenerateRoute: appRouter.generateRoute,
       ),
     );
